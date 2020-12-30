1c59c422b64e2bfb45c58e993c2c83b901502ed1
hashemi <masoumeh.hashemi92@gmail.com>
2020-12-30 18:16:35 +0330

    add posts to category

--- packages/server/src/schemas/blogCategory.ts
+++ packages/server/src/schemas/blogCategory.ts
@@ -1,12 +1,15 @@
 import { Base, baseSelectableFields, RBase } from "./utils/bases/index.ts";
 import db from "../../db.ts";
 import { fieldType, RType } from "./utils/index.ts";
+import { BlogPost, RBlogPost } from "./blogPost.ts";
+import { Bson } from "https://deno.land/x/mongo@v0.20.1/deps.ts";
 
 export interface BlogCategory extends Base {
 	name: string;
 	enName: string;
 	icon: string;
 	description: string;
+	blogPosts: BlogPost[];
 }
 
 export interface RBlogCategory extends RBase {
@@ -14,6 +17,7 @@ export interface RBlogCategory extends RBase {
 	enName?: RType;
 	iconUrl?: RType;
 	description?: RType;
+	blogPosts?: RBlogPost;
 }
 
 export const blogCategorySelectable = (depth: number = 4) => {
