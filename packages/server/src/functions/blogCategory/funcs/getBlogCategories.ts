0faef5812ad1ef95324b747a554d048a52b801d0
hashemi <masoumeh.hashemi92@gmail.com>
2020-12-30 18:15:13 +0330

    blogCategory funcs

---
+++ packages/server/src/functions/blogCategory/funcs/getBlogCategories.ts
@@ -0,0 +1,33 @@
+import { getBlogPosts } from "./../../blogPost/funcs/getBlogPosts.ts";
+import { populateMany } from "./../../../utils/populateMany.ts";
+import { Bson } from "https://deno.land/x/mongo@v0.20.1/deps.ts";
+import { makeProjections } from "../../../utils/makeProjections.ts";
+import {
+	blogCategories,
+	BlogCategory,
+	RBlogCategory,
+} from "./../../../schemas/blogCategory.ts";
+
+type GetBlogCategoriesInput = { filter: Bson.Document; getObj: RBlogCategory };
+type GetBlogCategoriesFn = ({
+	filter,
+	getObj,
+}: GetBlogCategoriesInput) => Promise<BlogCategory[]>;
+export const getBlogCategories: GetBlogCategoriesFn = async ({
+	filter,
+	getObj,
+}) => {
+	const projection = makeProjections(getObj, ["blogPost"], []);
+	const foundedBlogCategories = await blogCategories.find(filter, {
+		projection,
+	});
+	let returnBlogCategories = await foundedBlogCategories.toArray();
+	if (getObj.blogPosts)
+		returnBlogCategories = await populateMany(
+			returnBlogCategories,
+			getBlogPosts,
+			"states",
+			getObj.blogPosts
+		);
+	return returnBlogCategories;
+};
---
+++ packages/server/src/functions/blogCategory/funcs/getBlogCategory.ts
@@ -0,0 +1,31 @@
+import { getBlogPosts } from "./../../blogPost/funcs/getBlogPosts.ts";
+import { makeProjections } from "./../../../utils/makeProjections.ts";
+import { RBlogCategory } from "./../../../schemas/blogCategory.ts";
+import { blogCategories, BlogCategory } from "../../../schemas/blogCategory.ts";
+import { throwError } from "../../../utils/throwErr.ts";
+import { Bson } from "https://deno.land/x/mongo@v0.20.1/deps.ts";
+
+type GetBlogCategoryInput = { _id: Bson.ObjectID; get: RBlogCategory };
+type GetBlogCategoryFn = ({
+	_id,
+	get,
+}: GetBlogCategoryInput) => Promise<BlogCategory>;
+
+export const getBlogCategory: GetBlogCategoryFn = async ({ _id, get }) => {
+	const projection = makeProjections(get, ["blogPost"], []);
+	const foundedBlogCategory = await blogCategories.findOne(
+		{ _id },
+		{ projection }
+	);
+	const doRelation = async (blogCategory: BlogCategory, get: RBlogCategory) => {
+		if (get.blogPosts)
+			blogCategory.blogPosts = await getBlogPosts({
+				filter: { blogCategory: blogCategory._id },
+				getObj: get.blogPosts,
+			});
+		return blogCategory;
+	};
+	return foundedBlogCategory
+		? await doRelation(foundedBlogCategory, get)
+		: throwError("can not find blogCategory");
+};
