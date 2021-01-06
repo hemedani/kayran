9749ca3524683aa42652b3075247debe08e63d62
hashemi <masoumeh.hashemi92@gmail.com>
2021-01-06 11:33:29 +0330

    fixing create blog comment bug

--- packages/server/src/functions/blogComment/createBlogComment.ts
+++ packages/server/src/functions/blogComment/createBlogComment.ts
@@ -11,6 +11,7 @@ import { throwError } from "../../utils/throwErr.ts";
 import FastestValidator from "https://cdn.pika.dev/fastest-validator@^1.8.0";
 import { getBlogComment } from "./funcs/getBlogComment.ts";
 import { User } from "../../schemas/user.ts";
+import { blogPosts } from "../../schemas/blogPost.ts";
 // TODO:Check it with satek
 const v = new FastestValidator();
 const check = v.compile({
@@ -23,13 +24,13 @@ const check = v.compile({
 					name: { type: "string" },
 					email: { type: "email" },
 					content: { type: "string" },
-					blogPost: { type: "string" },
+					blogPostId: { type: "string" },
 					blogCommentStatus: {
 						type: "enum",
 						values: ["ACCEPT", "PENDING", "REJECT"],
 					},
-					isReplierBlogComment: { type: "boolean" },
-					repliedBlogCommentId: { type: "string" },
+					isReplierBlogComment: { type: "boolean", optional: true },
+					repliedBlogCommentId: { type: "string", optional: true },
 				},
 			},
 			get: {
@@ -41,12 +42,12 @@ const check = v.compile({
 	},
 });
 
-interface createBlogCommentDetails {
+export interface createBlogCommentDetails {
 	set: {
 		name: string;
 		email: string;
 		content: string;
-		blogPost: string;
+		blogPostId: string;
 		blogCommentStatus: BlogCommentStatus;
 		isReplierBlogComment: boolean;
 		repliedBlogCommentId?: string;
@@ -78,37 +79,58 @@ export const createBlogComment: CreateBlogComment = async (
 			name,
 			email,
 			content,
-			blogPost,
+			blogPostId,
 			blogCommentStatus,
 			isReplierBlogComment,
 			repliedBlogCommentId,
 		},
 		get,
 	} = details;
+	if (
+		(isReplierBlogComment && !repliedBlogCommentId) ||
+		(!isReplierBlogComment && repliedBlogCommentId)
+	) {
+		throw new Error(
+			"if this comment is replied to another comment, the id of the parent comment should be specified, and if this comment is not a reply it should not have the parent Id"
+		);
+	}
+	/**
+	 * step1: creating the new comment in db, with all the fields even if empty
+	 */
 
-	const createdBlogComment = await blogComments.insertOne({
+	const createdBlogCommentID = await blogComments.insertOne({
 		name,
 		email,
 		content,
-		blogPost, //this is the id of blogPost
+		blogPostId, //this is the id of blogPost
 		blogCommentStatus,
 		isReplierBlogComment,
 		repliedBlogCommentId,
 	});
 
-	const ob = new Bson.ObjectID(createdBlogComment);
-	// await blogPosts.updateOne(
-	// 	{ _id: blogPost },
-	// 	{ $addToSet: { blogComments: createdBlogComment } }
-	// );
+	/**
+	 * step2: adding the new comment to post document
+	 */
+
+	const createdBlogComment = await blogComments.findOne({
+		_id: new Bson.ObjectID(createdBlogCommentID),
+	});
+	await blogPosts.updateOne(
+		{ _id: new Bson.ObjectID(blogPostId) },
+		{ $addToSet: { blogComments: createdBlogComment } }
+	);
+	/**
+	 * step3: if the comment is replied to another comment, add the child commentId to the replierBlogCommentRefs of the parent
+	 */
 
 	isReplierBlogComment
 		? await blogComments.updateOne(
-				{ _id: repliedBlogCommentId },
+				{ _id: new Bson.ObjectID(repliedBlogCommentId) },
 				{
-					$addToSet: { replierBlogCommentRefs: createdBlogComment._id },
+					$addToSet: { replierBlogCommentRefs: createdBlogCommentID },
 				}
 		  )
 		: null;
+	const ob = new Bson.ObjectID(createdBlogCommentID);
 	return get ? getBlogComment({ _id: ob, get }) : { _id: ob };
 };
