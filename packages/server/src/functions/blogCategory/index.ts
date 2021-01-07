261ac54368a27212394388c61acfe4bd9151ea09
hashemi <masoumeh.hashemi92@gmail.com>
2021-01-07 11:38:51 +0330

    deleteBLogPost

--- packages/server/src/functions/blogCategory/index.ts
+++ packages/server/src/functions/blogCategory/index.ts
@@ -1,16 +1,20 @@
 import FastestValidator from "https://cdn.pika.dev/fastest-validator@^1.8.0";
 import { throwError } from "../../utils/throwErr.ts";
 import { createBlogCategory } from "./createBlogCategory.ts";
+import { deleteBlogCategory } from "./deleteBlogCategory.ts";
 import { updateBlogCategory } from "./updateBlogCategory.ts";
 const v = new FastestValidator();
 const check = v.compile({
 	doit: {
 		type: "enum",
-		values: ["createBlogCategory", "updateBlogCategory"],
+		values: ["createBlogCategory", "updateBlogCategory", "deleteBlogCategory"],
 	},
 });
 
-export type BlogCategoryDoit = "createBlogCategory" | "updateBlogCategory";
+export type BlogCategoryDoit =
+	| "createBlogCategory"
+	| "updateBlogCategory"
+	| "deleteBlogCategory";
 
 type BlogCategoryFns = (
 	doit: BlogCategoryDoit,
@@ -26,6 +30,8 @@ export const blogCategoryFns: BlogCategoryFns = (doit, details, context) => {
 					await createBlogCategory(details, context),
 				["updateBlogCategory"]: async () =>
 					await updateBlogCategory(details, context),
+				["deleteBlogCategory"]: async () =>
+					await deleteBlogCategory(details, context),
 		  }[doit]()
 		: throwError(checkDoit[0].message);
 };
