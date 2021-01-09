8527ee005de07fd5d85117019db0e7bd866ad5f8
hashemi <masoumeh.hashemi92@gmail.com>
2021-01-09 12:55:41 +0330

    blogComment Index

--- packages/server/src/functions/blogComment/index.ts
+++ packages/server/src/functions/blogComment/index.ts
@@ -1,16 +1,20 @@
 import FastestValidator from "https://cdn.pika.dev/fastest-validator@^1.8.0";
 import { throwError } from "../../utils/throwErr.ts";
 import { createBlogComment } from "./createBlogComment.ts";
+import { deleteBlogComment } from "./deleteBlogComment.ts";
 import { updateBlogComment } from "./updateBlogComment.ts";
 const v = new FastestValidator();
 const check = v.compile({
 	doit: {
 		type: "enum",
-		values: ["createBlogComment", "updateBlogComment"],
+		values: ["createBlogComment", "updateBlogComment", "deleteBlogComment"],
 	},
 });
 
-export type BlogCommentDoit = "createBlogComment" | "updateBlogComment";
+export type BlogCommentDoit =
+	| "createBlogComment"
+	| "updateBlogComment"
+	| "deleteBlogComment";
 
 type BlogCommentFns = (
 	doit: BlogCommentDoit,
@@ -26,6 +30,8 @@ export const blogCommentFns: BlogCommentFns = (doit, details, context) => {
 					await createBlogComment(details, context),
 				["updateBlogComment"]: async () =>
 					await updateBlogComment(details, context),
+				["deleteBlogComment"]: async () =>
+					await deleteBlogComment(details, context),
 		  }[doit]()
 		: throwError(checkDoit[0].message);
 };
